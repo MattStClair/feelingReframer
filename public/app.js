d7f3a98e54eddad0086c18a763be4dd7417ac2a2
mstclair <stclairm@spu.edu>
2017-10-23 19:51:39 -0700

    removing array pieces

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/', method: 'GET'}).then(console.log);
+//$.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/', method: 'GET'}).then(console.log);
 //
 // $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/sad/', method: 'GET'}).then(console.log);
 //
@@ -10,29 +10,35 @@
 // var myJSON = $.get({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/json', method: 'GET'});
 //
 // var myObj = JSON.parse(myJSON);
-//
-//
-// document.getElementById("demo").innerHTML = myObj.name;
-//var object = $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/JSON', method: 'GET'});
 
-// var object = $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/', method: 'GET'}).then(function(nameOfStuffComingBack)
-// {for(var i = 0; i < nameOfStuffComingBack.length; i++){console.log(nameOfStuffComingBack[i])}});
-//
-// $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/', method: 'GET'}).then(function(nameOfStuffComingBack)
-//    {
-//   for(var i = 0; i < nameOfStuffComingBack.length; i++)
-//       {
-//     console.log(nameOfStuffComingBack[i]);
-//
-//   }
-//
-//    var output = document.getElementById('output');
-//
-//    output.innerHTML = 'stuff is there' + nameOfStuffComingBack;
-//
-//    console.log(nameOfStuffComingBack);
-// });
+var str = '';
 
+
+var objectArray = $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/', method: 'GET'}).then(function(nameOfStuffComingBack)
+{
+  for(var i = 0; i < nameOfStuffComingBack.length; i++){
+
+    str = str + nameOfStuffComingBack[i];};
+
+
+});
+
+$.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/happy/', method: 'GET'}).then(function(nameOfStuffComingBack)
+   {
+  for(var i = 0; i < nameOfStuffComingBack.length; i++)
+      {
+    //console.log(str = str + nameOfStuffComingBack[i]);
+
+  }
+
+  var output = document.getElementById('output');
+  var res = str.split('|');
+  var rep = str.split('adjective');
+  var newArray = res.concat(rep);
+  output.innerHTML = 'stuff is there' + newArray;
+
+  //console.log(nameOfStuffComingBack);
+});
 
 
 //console.log(object);
@@ -49,9 +55,6 @@
 //
 // console.log(array1);
 //need to access this object...some how
-
-console.log("what the hell?");
-
 
 
 function showHappyWords(){

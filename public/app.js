cdb54ea54a306055e8ffcb4820ebcf5470a94f78
mstclair <stclairm@spu.edu>
2017-11-06 08:50:49 -0800

    added chart, about me, and links to data and about me

--- public/app.js
+++ public/app.js
@@ -1,88 +1,156 @@
 'use strict';
-//
-// var word = $('#wordSearch');
+
 var str = '';
 
-
-//console.log(text);
-
-
+var clicks = 0;
+var ctx = document.getElementById('myChart').getContext('2d');
 var word = 'sad';
 
 
 
 console.log(word);
 
-//var word = document.getElementById('wordSearch');///get elementby id
-
 function getResults() {
 
-//this resquest creates the words===========================>
-var objectArray = $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/' + word + '/', method: 'GET'}).then(function(nameOfStuffComingBack)
-{
-  for(var i = 0; i < nameOfStuffComingBack.length; i++){
+  //this resquest creates the words===========================>
+  var objectArray = $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/' + word + '/', method: 'GET'}).then(function(nameOfStuffComingBack)
+  {
+    for(var i = 0; i < nameOfStuffComingBack.length; i++){
 
-    str = str + nameOfStuffComingBack[i];};
-});
+      str = str + nameOfStuffComingBack[i];};
+    });
 
-//this request edits them=====================================>
-  $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/' + word + '/', method: 'GET'}).then(function(nameOfStuffComingBack)
-   {
-  for(var i = 0; i < nameOfStuffComingBack.length; i++)
+    //this request edits them=====================================>
+    $.ajax({url: 'http://words.bighugelabs.com/api/2/dd5bfddecdc1451b1ba9c7ac2ed20f4e/' + word + '/', method: 'GET'}).then(function(nameOfStuffComingBack)
+    {
+      for(var i = 0; i < nameOfStuffComingBack.length; i++)
       {
-    var rep = str.replace(/adjective|syn|rel|sim|unhappy|ant|\W+/g, ' ');
+        var rep = str.replace(/adjective|syn|rel|sim|unhappy|ant|\W+/g, ' ');
       }
 
-  var output = document.getElementById('output');
-  output.innerHTML = 'Other feelings are:  ' + rep;
+      var output = document.getElementById('output');
+      output.innerHTML = 'Other feelings are:  ' + rep;
 
-  //console.log(nameOfStuffComingBack);
-});
+      //console.log(nameOfStuffComingBack);
+    });
+
+  }
+
+  function showData(){
+    $('.dataSummary').hide();
+    $('#dataSumClick').click(function(){
+      $('.dataSummary').show(1000);
+      $('.feelingHome').hide();
+      $('.about').hide();
+      console.log('it works');
+    });
+  }
+
+  function showHome(){
+    $('#homeLink').click(function(){
+      $('.feelingHome').show(1000);
+      $('.dataSummary').hide();
+      $('.about').hide();
+    });
+  }
+
+  function showAbout(){
+    $('.about').hide();
+    $('#aboutClick').click(function(){
+      $('.about').show(1000);
+      $('.dataSummary').hide();
+      $('.feelingHome').hide();
+
+    });
 
 
-}
+  }
 
-function showHappyWords(){
-  $('.happyList').hide();
-  $('.happybutton').click(function() {
-    $('.happyList').toggle(1000);
+  function showHappyWords(){
+    $('.happyList').hide();
+    $('.happybutton').click(function() {
+      $('.happyList').toggle(1000);
+      clicks += 1;
+
+      localStorage.setItem('localCount', 'clicks');
+
+      console.log(clicks);
+    });
+
+  }
+
+  function showSadWords(){
+    $('.sadList').hide();
+    $('.sadbutton').click(function() {
+      $('.sadList').toggle(1000);
+    });
+
+  }
+
+  function showAngryWords(){
+    $('.angryList').hide();
+    $('.angrybutton').click(function() {
+      $('.angryList').toggle(1000);
+    });
+
+  }
+  function showScaredWords(){
+    $('.scaredList').hide();
+    $('.scaredbutton').click(function() {
+      $('.scaredList').toggle(1000);
+    });
+
+  }
+
+  document.getElementById('searchButton').addEventListener('click', function() {
+    // display the current click count inside the clicked div
+    getResults();
+
+  },false);
+
+  var stuff = localStorage.getItem('localCount');
+
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Happy", "Sad", "Angry", "Scared"],
+      datasets: [{
+        label: '# of clicks',
+        data: [10,5,4,6],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
   });
 
-}
-
-function showSadWords(){
-  $('.sadList').hide();
-  $('.sadbutton').click(function() {
-    $('.sadList').toggle(1000);
-  });
-
-}
-
-function showAngryWords(){
-  $('.angryList').hide();
-  $('.angrybutton').click(function() {
-    $('.angryList').toggle(1000);
-  });
-
-}
-function showScaredWords(){
-  $('.scaredList').hide();
-  $('.scaredbutton').click(function() {
-    $('.scaredList').toggle(1000);
-  });
-
-}
-
-document.getElementById('searchButton').addEventListener('click', function() {
-// display the current click count inside the clicked div
-  getResults();
-
-},false);
-
-
-
+showHome();
+showData();
 showAngryWords();
 showScaredWords();
-
+showAbout();
 showSadWords();
 showHappyWords();
